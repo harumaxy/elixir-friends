742576ce76bfd45c2b469d1aae61bf850840c2dd
harumaxy <harumaxy@gmail.com>
2024-12-10 01:27:11 +0900

    update

--- lib/friends/movie.ex
+++ lib/friends/movie.ex
@@ -4,7 +4,8 @@ defmodule Friends.Movie do
   schema "movies" do
     field(:title, :string)
     field(:tagline, :string)
-    has_many(:characters, Friend.Character)
+    has_many(:characters, Friends.Character)
     has_one(:distributor, Friends.Distributor)
+    many_to_many(:actors, Friends.Actor, join_through: "movies_actors")
   end
 end
